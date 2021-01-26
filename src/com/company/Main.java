a309e29cf07e4a0a51e145286afbac0d6763d5fe
Michael Brave <michaelbrave@gmail.com>
2021-01-26 16:08:52 -0600

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+public static void ask() {
+    Scanner scanner = new Scanner(System);
+    System.out.println("please input 5 numbers");
+    int[] numbers;
+    for (int i= 0; i < 6; i++) {
+        System.out.printf("please enter number %d", i);
+        
+    }
+
+}
